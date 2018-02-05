8b64dcb0bc4974bebc59e855716c19247ced1d73
elli.g.pope <elli.g.pope@gmail.com>
2018-02-04 23:21:47 -0500

    changes, who cares

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,8 @@
 // global variables, keeps a count for behaviors and master ABD
 
 var count = 1000;
-var BASE_URL = 'http://lantern-lizzypresland291247.codeanyapp.com/scripts/getValues.py';
+var invocation = new XMLHttpRequest();
+var BASE_URL = 'http://lantern-elligpope365078.codeanyapp.com/scripts/getValues.py';
 
 var ABD = ['Slurred speech','Poor academic performance','Signs of hangover (bloodshot eyes, changes in pupils)','Changes in eating habits','Tremors or impaired coordination','High risk behaviors','Poor school attendance','Appears fearful, anxious, or paranoid','Sudden mood swings','Aggressive behavior','Unexplained hyperactivity','Lack of motivation','Excessive moodiness and tears','Anger and irritability','Excessive sensitivity to criticism','Difficulty concentrating','Changes in sleep patterns','Thoughts of suicide','Self-mutilation (cutting)','Feelings of worthlessness and/or helplessness','Changes in eating patterns that result in dramatic weight gain or loss','Body image issues','Unexplained aches and pains','Social isolation, abandonment of peer group','Secrecy','Isolation from family members','Excessive sleeping beyond your child’s normal fatigue or insomnia','Sudden changes in academic performance','Dramatic changes in eating habits','Loss of interest in normal activities','Social isolation','Changes in personality (becomes more aggressive, angry, withdrawn, etc.)','Make changes in their daily rituals?','Retreat from school or activities?','Experience isolation from friends?','Show a dramatic in weight, appearance and/or grades?','Wear clothing inappropriate for the weather (possibly to hide marks)?','Have visible marks or bruises?','Spend excessive amounts of time with the person they’re dating?','Spend excessive amounts of time in contact with the person they are dating through cell phones and computers?','Excessive sleeping, beyond usual teenage fatigue, which could indicate depression or substance abuse; difficulty in sleeping, insomnia, and other sleep disorders *','Loss of self-esteem','Abandonment or loss of interest in favorite pastimes','Unexpected and dramatic decline in academic performance','Weight loss and loss of appetite, which could indicate an eating disorder','Personality shifts and changes, such as aggressiveness and excess anger that are sharply out of character and could indicate psychological, drug, or sexual problems','Changes in sleep patterns','Unexpected weeping or excessive moodiness','Eating habits that result in noticeable weight loss or gain','Expressions of hopelessness or worthlessness','Paranoia and excessive secrecy','Self-mutilation, or mention of hurting himself or herself','Obsessive body-image concerns','Excessive isolation','Abandonment of friends and social groups','Avoidance of food and noticeable changes in eating habits should trigger concern.','Purging (forced vomiting) after eating','Lying','Over-reactive arguing','An awkward phase', 'Defiance','Abandoning commitments','Withdrawal','Attitude. “Teenager” and “bad attitude”','Impulsivity','Academic problems','Curfew violations'];
 
@@ -113,7 +114,7 @@ function cleanPythonArray(arr){
     return self.indexOf(item) == pos;
   });
   
-  console.log(newArray);
+//  console.log(newArray);
   sendToPython(newArray);
 }
 
@@ -129,7 +130,7 @@ function sendToPython(input) {
       url: BASE_URL,
       data: sendData,
       success: function(data) {
-          var result = JSON.parse(data); 
+          var result = JSON.stringify(data); 
           dictArr.push(result);
           console.log("I'm listening to this python file right meow");
           console.log(result);
